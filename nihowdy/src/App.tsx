39ef70190e718df979ae4cd10d30f6d75314bd87
ParchmentRare <krc7uw@virginia.edu>
2026-03-22 00:56:55 -0400

    changed app.tsx to allow for auth0 verification

--- nihowdy/src/App.tsx
+++ nihowdy/src/App.tsx
@@ -3,19 +3,29 @@ import Navigation from '@/components/navigation'
 import StudyPlanDashboard from '@/components/study-plan-dashboard'
 import MaterialsContent from '@/components/materials-content'
 import { WeeklyPlan } from './components/weekly-plan'
+import {Auth0Provider} from "@auth0/auth0-react"
+import { StrictMode } from 'react'
 
 function App() {
   return (
-    <div className="min-h-screen bg-background">
-      <Navigation />
-      <main className="mx-auto w-full max-w-7.5xl px-4 sm:px-6 lg:px-8 py-6">
-        <Routes>
-          <Route path="/" element={<StudyPlanDashboard />} />
-          <Route path="/materials" element={<MaterialsContent />} />
-          <Route path="/weeklyPlan" element={<WeeklyPlan />} />
-        </Routes>
-      </main>
-    </div>
+    <StrictMode>
+      <Auth0Provider
+        domain="dev-5d72ymseihy5vc1o.us.auth0.com"
+        clientId="NazzAxLakglLksC63NZ5D8Kv22VJmXZa"
+        authorizationParams={{ redirect_uri: window.location.origin }}
+      >
+        <div className="min-h-screen bg-background">
+          <Navigation />
+          <main className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 py-6">
+            <Routes>
+              <Route path="/" element={<StudyPlanDashboard />} />
+              <Route path="/materials" element={<MaterialsContent />} />
+              <Route path="/weeklyPlan" element={<WeeklyPlan />} />
+            </Routes>
+          </main>
+        </div>
+      </Auth0Provider>
+    </StrictMode>
   )
 }
 
